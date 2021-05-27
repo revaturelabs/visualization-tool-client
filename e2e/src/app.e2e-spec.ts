2b7ef8e96f0e3bbf4fca94dc10faea5635e4c013
Bach Tran <bach.tran@revature.com>
2021-05-27 10:09:55 -0500

    Update default tests to pass based on initial template

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('project3-cvt app is running!');
+    expect(await page.getTitleText()).toEqual('Hello, World!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,4 +25,11 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('project3-cvt');
   });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Hello, World!');
+  });
 });
