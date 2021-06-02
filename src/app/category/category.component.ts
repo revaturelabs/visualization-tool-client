fa794febbe7c062d0fdc1d468dc84f5ca619cc01
Chakong Xiong <33104327+CommanderXiong@users.noreply.github.com>
2021-06-02 17:37:15 -0400

    Category frontend (#10)
    * Initial commit
    
    * Add flex to category items

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  categoryResults: any[] = new Array();
+  categoryList: any[] = [
+    {
+      id:1,
+      categoryName:"Language",
+      categoryDescription:"Programming Language",
+      categoryColor:"#e59372"
+    },
+    {
+      id:2,
+      categoryName:"DevOps",
+      categoryDescription:"Developement and Operations",
+      categoryColor:"#e59372"
+    },
+    {
+      id:3,
+      categoryName:"Frontend",
+      categoryDescription:"Client Side Frameworks",
+      categoryColor:"#e59372"
+    }
+  ];
+  constructor(private categoryService: CategoryService) { 
+    
   }
 
+  ngOnInit(): void {
+    //this.getAllCategories();
+
+    console.log("CategoryResult "+this.categoryResults);
+  }
+
+  getAllCategories(){
+    this.categoryService.getCategories().subscribe((result)=>{
+      console.log("Result "+result);
+      this.categoryResults = result;
+    });
+  }
+
+  randColor(){
+    let number = Math.floor(Math.random()*16777215).toString(16);
+    console.log(number);
+    return "#"+number;
+  }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  apiURL = 'http://3.226.243.38:8081';
+  apiURL = 'http://3.226.243.38:8081/category/';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
 
   constructor(private httpClient: HttpClient) { }
 
+  getCategories():Observable<[]>{
+    return this.httpClient.get<[]>(this.apiURL);
+  }
 
+  addCategory(bodyObject: object):Observable<object>{
+    return this.httpClient.post<object>(this.apiURL,bodyObject, this.httpOptions);
+  }
 
+  updateCategory(id: number, bodyObject: object):Observable<object>{
+    return this.httpClient.put<object>(this.apiURL+id,bodyObject, this.httpOptions);
+  }
+
+  deleteCategory(id: number){
+    return this.httpClient.delete(this.apiURL+id);
+  }
 }
